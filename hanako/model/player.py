5377c7a0a8a165207d9122de5584c026cb21f873
Zoran Pandovski <zoran.pandovski@keitaro.com>
2018-11-30 20:41:47 +0100

    Add player and Room models

---
+++ hanako/model/player.py
@@ -0,0 +1,25 @@
+class Player:
+    """Player class"""
+    
+    def __init__(self, name):
+        self.name = name
+
+    def __str__():
+        info = "User %s", self.info
+        return info
+
+    def __repr__():
+        return "User"
+
+class Room:
+    """ Room class"""
+
+    def __init__(self, name):
+        self.name = name
+    
+     def __str__():
+        info = "Room %s", self.info
+        return info
+
+    def __repr__():
+        return "Room"
